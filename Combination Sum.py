e66df9c57ae34a23eb8f0e44c78cdb217e96d478
Shakespere <shakespere@zju.edu.cn>
2019-04-24 23:58:45 +0800

    Add files via upload

---
+++ Combination Sum.py
@@ -0,0 +1,51 @@
+# @Time    : 2019/4/24 23:57
+# @Author  : shakespere
+# @FileName: Combination Sum.py
+'''
+39. Combination Sum
+Medium
+Given a set of candidate numbers (candidates) (without duplicates) and a target number (target), find all unique combinations in candidates where the candidate numbers sums to target.
+
+The same repeated number may be chosen from candidates unlimited number of times.
+
+Note:
+
+All numbers (including target) will be positive integers.
+The solution set must not contain duplicate combinations.
+Example 1:
+
+Input: candidates = [2,3,6,7], target = 7,
+A solution set is:
+[
+  [7],
+  [2,2,3]
+]
+Example 2:
+
+Input: candidates = [2,3,5], target = 8,
+A solution set is:
+[
+  [2,2,2,2],
+  [2,3,3],
+  [3,5]
+]
+'''
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        candidates.sort()
+        res = []
+        def DFS(candidate,target,start,vallist):
+            if target == 0:
+                res.append(vallist)
+                return res
+            for i in range(start,len(candidate)):
+                if target<candidates[i]:
+                    return
+                DFS(candidates,target-candidate[i],i,vallist+[candidate[i]])
+        DFS(candidates,target,0,[])
+        return res
