19904f3b99e2b81af753311cdc2d7e88739d8153
Shakespere <shakespere@zju.edu.cn>
2019-06-24 23:24:03 +0800

    Add files via upload

---
+++ Implement Trie (Prefix Tree).py
@@ -0,0 +1,74 @@
+# @Time    : 2019/6/24 23:21
+# @Author  : shakespere
+# @FileName: Implement Trie (Prefix Tree).py
+'''
+208. Implement Trie (Prefix Tree)
+Medium
+Implement a trie with insert, search, and startsWith methods.
+
+Example:
+
+Trie trie = new Trie();
+
+trie.insert("apple");
+trie.search("apple");   // returns true
+trie.search("app");     // returns false
+trie.startsWith("app"); // returns true
+trie.insert("app");
+trie.search("app");     // returns true
+Note:
+
+You may assume that all inputs are consist of lowercase letters a-z.
+All inputs are guaranteed to be non-empty strings.
+'''
+
+
+class Node(object):
+    def __init__(self):
+        self.children = collections.defaultdict(Node)
+        self.isword = False
+
+
+class Trie(object):
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = Node()
+
+    def insert(self, word):
+        """
+        Inserts a word into the trie.
+        :type word: str
+        :rtype: void
+        """
+        current = self.root
+        for w in word:
+            current = current.children[w]
+        current.isword = True
+
+    def search(self, word):
+        """
+        Returns if the word is in the trie.
+        :type word: str
+        :rtype: bool
+        """
+        current = self.root
+        for w in word:
+            current = current.children.get(w)
+            if current == None:
+                return False
+        return current.isword
+
+    def startsWith(self, prefix):
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        :type prefix: str
+        :rtype: bool
+        """
+        current = self.root
+        for w in prefix:
+            current = current.children.get(w)
+            if current == None:
+                return False
+        return True
